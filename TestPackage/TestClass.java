cd6c5cfedfd5d899a0efe8bacd2b826674455ecd
Lin <645927031@qq.com>
2019-10-15 17:59:33 +0800

    Good night

---
+++ TestPackage/TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass{
+    public void TestClass(){
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        TestClass test = new TestClass();
+    }
+}
