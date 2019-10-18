d50bc0e12aa9e6a972b1091d94ab6324e39199a7
Anthony <125139205@qq.com>
2019-10-18 14:33:21 +0800

    remove lin

--- TestPackage/TestClass.java
+++
@@ -1,9 +0,0 @@
-public class TestClass{
-    public void TestClass(){
-        System.out.println("Hello world!");
-    }
-
-    public static void main(String[] args) {
-        TestClass test = new TestClass();
-    }
-}
